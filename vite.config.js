17fd25575db9a2a5aae4498bb95d0ba33aefd6a9
vibstaff725 <vibstaff725@gmail.com>
2026-03-04 10:27:47 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/market-intelligence-studio-project/'
+  base: '/market-intelligence-studio-project/',
+  plugins: [react(), tailwindcss()]
 })
